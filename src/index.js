f1c686ca82685257a44f539f50728dcecddebf77
Inna-99 <https://github.com/Inna-99/theyalow>
2019-10-08 17:51:36 +0300

    feat: add solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var a = str.length+1;
+      while(str = str.replace(/\<\>|\[\]|\{\}|\(\)|\|\||\{\}|[1][2]|[3][4]|[5][6]|[7][7]|[8][8]/,'')){
+          console.log(str);
+          if(str.length == a && a != 0) return false;
+          if(a == 0) return true;
+          a = str.length;
+      }
+      return true;
 }
+ 
